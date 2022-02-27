3c25b74028742330d3a451fcc834d88f62c256eb
MartijnKempen <martijn_kempen@live.nl>
2022-02-27 19:04:32 +0100

    Bugfix

--- Doser/forms.py
+++ Doser/forms.py
@@ -6,12 +6,7 @@ class ScheduleForm(forms.ModelForm):
     class Meta:
         model = DoseSchedule
         exclude=()
-<<<<<<< HEAD
 #       widgets = {
-=======
-        hidden=('doseToSchedule')
-#        widgets = {
->>>>>>> 11a40016eafd4ff8e3bf12a9fc896ba59327416e
 #            'doseToSchedule': Textarea(attrs={'cols': 80, 'rows': 20}),
 #        }
     def __init__(self, *args, **kwargs):
